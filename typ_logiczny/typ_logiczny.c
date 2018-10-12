ff35b7c9d7ccc2ae784ab4517959ab8895d5831f
Marcin <marcin.beldzinski@gmail.com>
2018-10-12 10:03:49 +0200

    utworzono typ logiczny

--- typ_logiczny/typ_logiczny.c
+++ typ_logiczny/typ_logiczny.c
@@ -2,14 +2,14 @@
 #include <stdbool.h>
 #include "stdio.h"
 int main () {
-enum dni {
-    ni, po = 5, wt, sr,czw, pi, so
-};
-enum dni dzien_tygodnia;
-bool czy_byl_obecny= false;
+    enum dni {
+        ni, po = 5, wt, sr, czw, pi, so
+    };
+    enum dni dzien_tygodnia;
+    bool czy_byl_obecny = false;
 
-dzien_tygodnia = so;
-printf ("wynik: %i\n",dzien_tygodnia);
+    dzien_tygodnia = so;
+    printf("wynik: %i\n", dzien_tygodnia);
 
-printf ("%i\n", czy_byl_obecny);
+    printf("%i\n", czy_byl_obecny);
 }
