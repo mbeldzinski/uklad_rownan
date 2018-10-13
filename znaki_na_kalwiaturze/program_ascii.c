37714aba4f04bb71034c6ffba9660da65885f0f7
Marcin <marcin.beldzinski@gmail.com>
2018-10-13 08:31:04 +0200

    wyświetla kod ascii zamiast znaku

---
+++ znaki_na_kalwiaturze/program_ascii.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+
+int main ()
+{
+
+    int kod_ascii;
+    printf("Podaj znak lub liczbę\n");
+    kod_ascii = getchar();
+    printf("kod ASCII: %d", kod_ascii);
+    return 0;
+
+6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
